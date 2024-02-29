87ea10c9ae8edbdc19ba1ea6309182506352fc21
Ximena <ximena.velasco@cceo.com.mx>
2024-02-29 09:50:53 -0600

    Directiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,23 @@
 <script setup>
-const name = "Vue dinámico";
-const activo = false
-const arrayFrutas = [
-  {
-    name: "Manzana",
-    price: "$1.00",
-    description: "Una manzana",
-    stock: 0,
-  },
-  {
-    name: "Pera",
-    price: "$2.00",
-    description: "Una pera",
-    stock: 10,
-  },
-  {
-    name: "Naranja",
-    price: "$3.00",
-    description: "Una naranja",
-    stock: 20,
-  },
-];
-
+  const name = 'Vue dinámico';
+  const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
 </script>
 
-<template>
+
+<template> 
   <h1>Hola {{ name.toUpperCase() }}</h1>
   <ui>
-    <li v-if="item.stock > 0" v-for="item in arrayFrutas" :key="item.name">
-      {{ item.name }} - {{ item.price }}
-    </li>
+    <li
+      v-for="(fruta, index) in arrayFrutas"
+      :key="index"
+      >{{ index }} - {{ fruta }}
+  </li>
   </ui>
 </template>
 
 
 <style>
-h1 {
-  color: rgb(234, 0, 255);
-}
+  h1 {
+    color: rgb(234, 0, 255);
+  }
 </style>
