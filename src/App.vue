ac34b7049ca5edb4da34986af5f98d4bb6fa2b09
Ximena <ximena.velasco@cceo.com.mx>
2024-02-29 09:56:21 -0600

    Recorriendo objetos con vfor

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,26 @@
 <script setup>
-  const name = 'Vue dinámico';
-  const arrayFrutas = [
-        {
-            name: "Manzana",
-            price: "$1.00",
-            description: "Una manzana",
-        },
-        {
-            name: "Pera",
-            price: "$2.00",
-            description: "Una pera",
-        },
-        {
-            name: "Naranja",
-            price: "$3.00",
-            description: "Una naranja",
-        },
-    ];
+const name = "Vue dinámico";
+const objetoFruta = {
+  name: "Manzana",
+  price: "$1.00",
+  description: "Una manzana",
+  id: 1,
+};
 </script>
 
-
-<template> 
+<template>
   <h1>Hola {{ name.toUpperCase() }}</h1>
+  {{ objetoFruta }}
   <ui>
-    <li
-      v-for="fruta in arrayFrutas"
-      :key="fruta.name"
-    >
-      {{ fruta.name }} - {{ fruta.price}} - {{ fruta.description }}
+    <li v-for="(value, propiedad, index) in objetoFruta" :key="value">
+      {{index}} - {{propiedad}} : {{ value }}
     </li>
   </ui>
 </template>
 
 
 <style>
-  h1 {
-    color: rgb(234, 0, 255);
-  }
+h1 {
+  color: rgb(234, 0, 255);
+}
 </style>
