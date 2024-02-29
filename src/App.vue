24a48def7999c3f13a58544c9c26b562b96420c4
Ximena <ximena.velasco@cceo.com.mx>
2024-02-29 11:01:24 -0600

    Evento click von

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,20 @@
-<template>
-  <div>
-    <h2>Lista de usuarios:</h2>
-    <ul>
-      <template
-        v-for="(user, index) in users"
-        :key="user.id"
-      >
-        <li>
-          <h3>{{ user.name }}</h3>
-          <ul>
-            <template
-              v-for="(post, index) in user.posts"
-              :key="post.id"
-            >
-              <li>{{ post.title }}</li>
-            </template>
-          </ul>
-        </li>
-      </template>
-    </ul>
-</div>
-</template>
- 
 <script setup>
-const users = [
-  {
-    id: 1,
-    name: 'Juan',
-    posts: [
-      { id: 1, title: 'Mi primer post' },
-      { id: 2, title: 'Mi segundo post' },
-    ]
-  },
-  {
-    id: 2,
-    name: 'Maria',
-    posts: [
-      { id: 3, title: 'Mi tercer post' },
-      { id: 4, title: 'Mi cuarto post' },
-    ]
-  }
-]
+const name = "Vue dinámico";
+
+//metodo - methods
+const handleClick = () => {
+  console.log("me diste click");
+};
 </script>
+
+<template>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <button v-on:click="handleClick">Activame 1</button>
+  <button @click="handleClick">Activame 2</button>
+</template>
+
+<style>
+h1 {
+  color: rgb(234, 0, 255);
+}
+</style>
