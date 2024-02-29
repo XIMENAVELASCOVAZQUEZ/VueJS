63471e93b558e5591bb98b0cb282bb0e06322088
Ximena <ximena.velasco@cceo.com.mx>
2024-02-29 13:50:01 -0600

    Desafio reactividad

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,34 @@ const counter = ref(0);
 const increment = () => {
   counter.value++;
 };
+
+const decrement = () => {
+  counter.value--;
+};
+
+const reset = () => {
+  counter.value=0;
+};
 </script>
 
 <template>
   <h1>Hola {{ name.toUpperCase() }}</h1>
-  <h2>{{ counter }}</h2>
+  <h2 :class="counter > 0 ? 'positive' : 'negative' ">{{ counter }}</h2>
   <button @click="increment">Aumentar</button>
+  <button @click="decrement">Disminuir</button>
+  <button @click="reset">Reset</button>
 </template>
 
 <style>
 h1 {
   color: rgb(234, 0, 255);
+}
+
+.positive {
+  color: green;
+}
+
+.negative {
+  color: red;
 }
 </style>
